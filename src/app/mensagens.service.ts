19a78b3e63effc1fb74fd6515ecd5a2d92659fc1
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2024-06-14 21:35:07 -0300

    listagem de mesnsagens funcionanando mais precisa de melhorias

--- src/app/mensagens.service.ts
+++ src/app/mensagens.service.ts
@@ -10,10 +10,16 @@ export class MensagensService {
 
   constructor(private http:HttpClient,private router:Router) { }
 
-  registerMsgService(msg:string) {
+  registerMsgService(msg:string,autor:string) {
+    console.log(autor," chegando no metodo registerMsbService do serviço MensagensService ");
 
     console.log(msg," chegando no metodo registerMsgService do serviço MensagensService")
-    return this.http.post<any>(`${this.baseUrl}/mensgens`, {msg});
+    return this.http.post<any>(`${this.baseUrl}/mensgens`, {msg,autor});
+  }
+
+  getAllmessage(){
+    console.log("enviando solicitação para recuperar mensagens");
+    return this.http.get<any[]>(`${this.baseUrl}/mensgens`);
   }
 
 }
