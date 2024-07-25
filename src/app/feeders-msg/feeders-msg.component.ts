0709fcbe6e911e637c7f8559ae3f29290e1f270f
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2024-07-25 00:38:09 -0300

    se user desconectado, a lista de msg exibe as tres ultimas msg que foi registrada, com visibilidade public, user escolhe se a msg sera publica ou privada

--- src/app/feeders-msg/feeders-msg.component.ts
+++ src/app/feeders-msg/feeders-msg.component.ts
@@ -13,6 +13,7 @@ interface Message {
   autor: string;
   criada_em:Date;
   pontos: number;
+  visibilidade_msg:string;
 }
 
 
@@ -70,21 +71,29 @@ export class FeedersMsgComponent implements OnInit {
       this.isUserLoggedin = this.listUsers.isAuthenticated(); // a função isAutenticated retorn um true se o usuario tiver logado
       console.log("autenticated: ", this.isUserLoggedin)
       if (!this.isUserLoggedin) { // se o usuario não estiver logado:
-        data = data.slice(0, 3); // a lisata de msg sera limitada as 3 ultimas msg registrada, se o usr estiver logado essa linha de codigo sera ignorada
+        data = data.filter(message => message.visibilidade_msg ==='public')// filtra as msg pela visibilidade === public
+                   .sort((a,b)=>new Date(b.criada_em).getTime() -new Date(a.criada_em).getTime()) // essa linha Faz com que o array de mensagens seja ordenado de modo que as mensagens mais recentes apareçam primeiro. Ela converte as datas de criação das mensagens para milissegundos e as compara para determinar a ordem.
+                   .slice(0, 3); // a lisata de msg sera limitada as 3 ultimas msg registrada, se o usr estiver logado essa linha de codigo sera ignorada
+         
       }
       this.messages = data;
       console.log("LoadMSG add msg do DB\n no array messages\n tipado com a interface Message[] ", this.messages);
       // o codigo envolvido nos + e = é apenas um teste de captura da quantidade de like e deslike, para implementar o puntuação geral da msg
       //++++++++++++++++++++++++====================++++++++++++++++++++++++++++++++====================================
-      this.messages.forEach((data, index) => {
+      this.messages.forEach((data, index) => { // percorre o array de messages e implementa a PG da msg em cada index
         data.pontos = data.likes - data.deslikes; // aqui eu implemento a pontuação geral da msg, que ficara armazena n propriedade pontos da interface message
         // console.log("LoadMSG add likes do DB\n em uma variavel", data.pontos, " id ", data.id);
 
-        this.expandedMessages[index] = data.pontos >= 0
-      })
+        // expandedMessages armazena um valor true ou false: Expande ou contrai a Msg
+        this.expandedMessages[index] = data.pontos >= 0 ? true : false; // Se data.pontos for maior ou igual a zero:  a expressão expandedMessages[index] na posição do index retorna true, do contrario false.
+      //   if(data.pontos >=0){
+      //     this.expandedMessages[index]=true;
+      //   }else{
+      //     this.expandedMessages[index]= false;
+      //   }
+       })
       //++++++++++++++++++++++++====================++++++++++++++++++++++++++++++++====================================
       this.totalPages = Math.ceil(this.messages.length / this.itemsPerPage);
-      // this.expandedMessages = new Array(this.messages.length).fill(true);
     },
       error => {
         console.error('Erro ao recuperar feedbacks:', error);
